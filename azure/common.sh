14283a1ceb4e9c86b551a1e64635142516630a3a
Leo Chen <leo@harmony.one>
2018-08-22 02:46:13 -0700

    Add init script to launch azure instances using template
    Noted during my test I found the deployment will hit errors once a while
    
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ azure/common.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# init all the environment variables and configurations
+
+#login to azure using your credentials
+az account show 1> /dev/null
+
+if [ $? != 0 ];
+then
+    az login
+fi
+
+CONFIG=configs/azure-env.json
+JQ="jq -M"
+
+SUBSCRIPTION=$($JQ .subscriptionId $CONFIG | tr -d \")
+RG=$($JQ .resourceGroup $CONFIG | tr -d \")
+REGIONS=( $($JQ ' .regions | .[] ' $CONFIG | tr "\n" " " | tr -d \") )
---
+++ azure/instances.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source ./common.sh
+
+function usage
+{
+   ME=$(basename $0)
+   cat<<EOF
+
+Usage: $ME [Options] ACTION
+
+This script is used to manage instances in Azure
+
+Options:
+   -h          print this help message
+   -n          dryrun mode (default)
+   -G          do the real work
+   -r region   run in region (supported regions: ${REGIONS[@]})
+   -p profile  use profile config file
+
+Action:
+   list        list the vm launched
+   launch      launch instances
+   terminate   terminate instances
+
+Examples:
+   $ME -r eastus list
+
+EOF
+
+   exit 0
+}
+
+function do_launch_instance
+{
+   local config=configs/azure-$PROFILE.json
+
+   for region in "${REGIONS[@]}"; do
+      if [ -n "$REGION" ]; then
+         # run on specificed region
+         if [ "$region" != "$REGION" ]; then
+            continue
+         fi
+      fi
+
+      LOG=logs/$region.launch.$TS.log
+      echo > $LOG
+
+      local template=configs/vm-template.json
+      local parameters=configs/vm-parameters.json
+
+      NUM=$($JQ " .$region | .instancecount " $config)
+      TYPE=$($JQ " .$region | .instancetype" $config)
+      NSG=$($JQ " .$region | .nsg " $config)
+      VNET=$($JQ " .$region | .vnet " $config)
+      KEY=$($JQ " .$region | .pubkey " $config)
+      echo launching vms in $region/$NUM/$TYPE/$NSG/$VNET
+      echo
+
+      $DRYRUN az group deployment create --name "$region.deploy.$TS" --resource-group "$RG" --template-file "$template" --parameters "@${parameters}"
+
+# create vm one by one, this can be used as a fallback mechanism
+#   for i in $(seq 1 $NUM); do
+#      node="node${region}${i}"
+#      az vm create --subscription "$SUBSCRIPTION"  --resource-group "$RG" --name $node --nsg $NSG --subnet default --vnet-name $VNET --ssh-key-value $KEY --public-ip-address ${node}_ip --size $TYPE --image UbuntuLTS | tee -a $LOG
+#   done
+
+      echo
+   done
+}
+
+function do_list_instance
+{
+   for region in "${REGIONS[@]}"; do
+      if [ -n "$REGION" ]; then
+         # run on specificed region
+         if [ "$region" != "$REGION" ]; then
+            continue
+         fi
+      fi
+
+      LOG=logs/$region.list.$TS.log
+      echo > $LOG
+
+      if [ -n "$DRYRUN" ]; then
+         echo az vm list --resource-group $RG --subscription $SUBSCRIPTION --query '[].{name:name, ip:publicIps, size:hardwareProfile.vmSize}' -o tsv
+      else
+         az vm list --resource-group $RG --subscription $SUBSCRIPTION --query  '[].{name:name, ip:publicIps, size:hardwareProfile.vmSize}' -o tsv | tee -a $LOG
+      fi
+
+   done
+}
+
+function do_terminate_instance
+{
+   for region in "${REGIONS[@]}"; do
+      if [ -n "$REGION" ]; then
+         # run on specificed region
+         if [ "$region" != "$REGION" ]; then
+            continue
+         fi
+      fi
+
+      LOG=logs/$region.delete.$TS.log
+      echo > $LOG
+
+      ID=( $(az vm list --resource-group $RG --subscription $SUBSCRIPTION --query '[].id' -o tsv) )
+      if [ -n "$DRYRUN" ]; then
+         echo az vm delete --yes --no-wait --resource-group $RG --subscription $SUBSCRIPTION --ids "${ID[@]}"
+      else
+         az vm delete --yes --no-wait --resource-group $RG --subscription $SUBSCRIPTION --ids "${ID[@]}" | tee -a $LOG
+      fi
+
+   done
+
+}
+
+######################################################
+
+DRYRUN=echo
+TS=$(date +%Y%m%d.%H%M%S)
+REGION=
+PROFILE=small
+
+while getopts "hnGr:p:" option; do
+   case $option in
+      r) REGION=$OPTARG ;;
+      G) DRYRUN= ;;
+      p) PROFILE=$OPTARG ;;
+      h|?|*) usage;;
+   esac
+done
+
+shift $(($OPTIND-1))
+
+ACTION=$@
+
+if [[ ! -z $ACTION && ! -z $DRYRUN ]]; then
+   echo '***********************************'
+   echo "Please use -G to do the real work"
+   echo '***********************************'
+fi
+
+case $ACTION in
+   list)
+      do_list_instance ;;
+   launch)
+      do_launch_instance ;;
+   terminate)
+      do_terminate_instance ;;
+   *)
+      usage ;;
+esac
