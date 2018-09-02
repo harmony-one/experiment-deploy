4477ffceaae3746cd9a0a23ea7323c58c30f7bc1
Leo Chen <leo@harmony.one>
2018-09-02 00:55:25 +0000

    temp fix of path
    Signed-off-by: Leo Chen <leo@harmony.one>

--- aws-experiment-launch/download_log_from_leaders.sh
+++ aws-experiment-launch/download_log_from_leaders.sh
@@ -6,12 +6,12 @@ for address in $(cat ./leader_addresses.txt)
 do
     echo "trying to download from address $address"
     mkdir -p tmp/$address
-    scp -r -o "StrictHostKeyChecking no" -i ../keys/california-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
-    scp -r -o "StrictHostKeyChecking no" -i ../keys/frankfurt-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
-    scp -r -o "StrictHostKeyChecking no" -i ../keys/ireland-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
-    scp -r -o "StrictHostKeyChecking no" -i ../keys/ohio-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
-    scp -r -o "StrictHostKeyChecking no" -i ../keys/oregon-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
-    scp -r -o "StrictHostKeyChecking no" -i ../keys/singapore-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
-    scp -r -o "StrictHostKeyChecking no" -i ../keys/tokyo-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
-    scp -r -o "StrictHostKeyChecking no" -i ../keys/virginia-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
+    scp -r -o "StrictHostKeyChecking no" -i ./keys/california-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
+    scp -r -o "StrictHostKeyChecking no" -i ./keys/frankfurt-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
+    scp -r -o "StrictHostKeyChecking no" -i ./keys/ireland-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
+    scp -r -o "StrictHostKeyChecking no" -i ./keys/ohio-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
+    scp -r -o "StrictHostKeyChecking no" -i ./keys/oregon-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
+    scp -r -o "StrictHostKeyChecking no" -i ./keys/singapore-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
+    scp -r -o "StrictHostKeyChecking no" -i ./keys/tokyo-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
+    scp -r -o "StrictHostKeyChecking no" -i ./keys/virginia-key-benchmark.pem ec2-user@$address:/home/tmp_log/* ./tmp/$address/
 done
