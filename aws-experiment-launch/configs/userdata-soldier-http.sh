370425fb6398a8fd22bd338da8a09755776b0708
Leo Chen <leo@harmony.one>
2018-09-12 00:32:12 +0000

    add a userdata file to start solder in http mode
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ aws-experiment-launch/configs/userdata-soldier-http.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+mkdir -p /home/ec2-user
+cd /home/ec2-user
+
+BUCKET=unique-bucket-bin
+FOLDER=leo/
+
+TESTBIN=( txgen soldier benchmark commander go-commander.sh )
+
+for bin in "${TESTBIN[@]}"; do
+   curl http://${BUCKET}.s3.amazonaws.com/${FOLDER}${bin} -o ${bin}
+   chmod +x ${bin}
+done
+
+echo "* soft     nproc          65535" | sudo tee -a /etc/security/limits.conf
+echo "* hard     nproc          65535" | sudo tee -a /etc/security/limits.conf
+echo "* soft     nofile         65535" | sudo tee -a /etc/security/limits.conf
+echo "* hard     nofile         65535" | sudo tee -a /etc/security/limits.conf
+echo "root soft     nproc          65535" | sudo tee -a /etc/security/limits.conf
+echo "root hard     nproc          65535" | sudo tee -a /etc/security/limits.conf
+echo "root soft     nofile         65535" | sudo tee -a /etc/security/limits.conf
+echo "root hard     nofile         65535" | sudo tee -a /etc/security/limits.conf
+echo "session required pam_limits.so" | sudo tee -a /etc/pam.d/common-session
+
+IS_AWS=$(curl -s -I http://169.254.169.254/latest/meta-data/instance-type -o /dev/null -w "%{http_code}")
+if [ "$IS_AWS" != "200" ]; then
+# NOT AWS, Assuming Azure
+   PUB_IP=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2017-04-02&format=text")
+else
+   PUB_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+fi
+
+NODE_PORT=9000
+SOLDIER_PORT=1$NODE_PORT
+
+# Kill existing soldier/node
+fuser -k -n tcp $SOLDIER_PORT
+fuser -k -n tcp $NODE_PORT
+
+# Run soldier
+./soldier -ip $PUB_IP -port $NODE_PORT -http > soldier-${PUB_IP}.log 2>&1 &
