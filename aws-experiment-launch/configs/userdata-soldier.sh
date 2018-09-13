a065eba0f14c12136f538067c32424f187bd0c76
Minh Doan <minh@simple-rules.com>
2018-09-13 15:46:54 -0700

    system config from ziliqa to soldier nodes

--- aws-experiment-launch/configs/userdata-soldier.sh
+++ aws-experiment-launch/configs/userdata-soldier.sh
@@ -13,6 +13,13 @@ for bin in "${TESTBIN[@]}"; do
    chmod +x ${bin}
 done
 
+sudo sysctl net.core.somaxconn=1024
+sudo sysctl net.core.netdev_max_backlog=65536;
+sudo sysctl net.ipv4.tcp_tw_reuse=1;
+sudo sysctl -w net.ipv4.tcp_rmem='65536 873800 1534217728';
+sudo sysctl -w net.ipv4.tcp_wmem='65536 873800 1534217728';
+sudo sysctl -w net.ipv4.tcp_mem='65536 873800 1534217728';
+
 echo "* soft     nproc          65535" | sudo tee -a /etc/security/limits.conf
 echo "* hard     nproc          65535" | sudo tee -a /etc/security/limits.conf
 echo "* soft     nofile         65535" | sudo tee -a /etc/security/limits.conf
