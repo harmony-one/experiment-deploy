c980e0c69c772f28726a21dc1d3a37d912756f3c
Leo Chen <leo@harmony.one>
2018-09-15 05:44:15 +0000

    improved cal_tps.sh
    Signed-off-by: Leo Chen <leo@harmony.one>

--- aws-experiment-launch/cal_tps.sh
+++ aws-experiment-launch/cal_tps.sh
@@ -1,1 +1,27 @@
-for file in $(ls *leader*); do echo $file; cat $file | grep TPS | head -n 2 |  cut -f2 -d ":" | cut -f1 -d "," | awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }'; done
+#!/usr/bin/env bash
+
+FILES=( $(ls leader-*.log) )
+NUM_SHARDS=${#FILES[@]}
+SUM=0
+NUM_CONSENSUS=0
+
+declare -A TPS
+
+for f in "${FILES[@]}"; do
+   leader=$( echo $f | cut -f 2 -d\- )
+   num_consensus=$(grep TPS $f | wc -l)
+   if [ $num_consensus -gt 0 ]; then
+      avg_tps=$(grep TPS $f | cut -f 2 -d: | cut -f 1 -d , | awk '{s+=$1} END {print s/NR}')
+      printf -v avg_tps_int %.0f $avg_tps
+   else
+      avg_tps=0
+   fi
+   TPS[$leader]="$num_consensus, $avg_tps"
+   NUM_CONSENSUS=$(expr $NUM_CONSENSUS + $num_consensus )
+   SUM=$( expr $SUM + $avg_tps_int )
+done
+
+echo $NUM_SHARDS shards, $NUM_CONSENSUS consensus, $SUM total TPS
+for t in "${!TPS[@]}"; do
+   echo $t, ${TPS[$t]}
+done
