9bd96799e4475d3cbef0d216c306f66fe3b1e7d3
Leo Chen <leo@harmony.one>
2018-09-15 00:28:22 +0000

    add a script to launch power leaders only
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ aws-experiment-launch/launch-leaders-only.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+TAG=${WHOAMI:-USER}
+
+REGIONS=1,2,3,4,5,6,7,8
+NUM=3,3,3,3,3,3,3,4
+
+./create_solider_instances.py \
+--regions $REGIONS \
+--instances $NUM \
+--instancetype c5.xlarge \
+--profile harmony-ec2 \
+--tag ${TAG}-powerleader \
+--userdata configs/userdata-soldier-http.sh \
+--instance_output instance_output-leaders.txt \
+--instance_ids_output instance_ids_output-leaders.txt
+
+./collect_public_ips.py \
+--profile harmony-ec2 \
+--instance_output instance_output-leaders.txt \
+--file_output raw_ip-leaders.txt
