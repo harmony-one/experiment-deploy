ed4bbaaceec843a597185eed63936eb8fa35b6fe
Leo Chen <leo@harmony.one>
2018-09-16 23:57:51 +0000

    improve the soldier test script
    Signed-off-by: Leo Chen <leo@harmony.one>

--- experiment/soldier/test/test-soldier.sh
+++ experiment/soldier/test/test-soldier.sh
@@ -1,14 +1,60 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# this script is used to test local soldier http server
+# it sends REST api request to soldier
 
-TEST=$1
+IP=127.0.0.1
+PORT=9000
 
-baseurl=http://127.0.0.1:19000
+function usage
+{
+   ME=$(basename $0)
+   cat<<EOT
+
+$ME [OPTIONS] TEST
+
+OPTIONS:
+   -h             print this help message
+   -p port        port of the soldier (default: $PORT)
+   -i ip_address  IP address of the soldier (default: $IP)
+
+TEST:
+   upload         upload the distribution_config.txt to test bucket
+
+   init,ping,kill,update,config
+                  API test commands sent to the soldier
+EOT
+   exit 0
+}
+
+while getopts "p:hi:" option; do
+   case $option in
+      h) usage ;;
+      p) PORT=$OPTARG ;;
+      i) IP=$OPTARG ;;
+   esac
+done
+
+shift $(($OPTIND-1))
+
+TEST=$*
+
+if [ -z "$TEST" ]; then
+   usage
+fi
+
+baseurl=http://$IP:$PORT
 cmds=( ping update init kill config )
 
 declare -A testdata
 testdata[update]=update.json
 testdata[init]=init.json
 testdata[config]=config.json
+
+if [ "$TEST" == "upload" ]; then
+   aws s3 cp distribution_config.txt s3://unique-bucket-bin/ec2-user/distribution_config.txt --acl public-read
+   exit 0
+fi
 
 for cmd in "${cmds[@]}"; do
    if [ -n "$TEST" -a "$cmd" != "$TEST" ]; then
