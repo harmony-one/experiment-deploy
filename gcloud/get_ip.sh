3c1fdb8423e984625a4f176ef9bc571efb7621ba
Minh Doan <minh@simple-rules.com>
2018-09-18 16:59:05 -0700

    delete get_ip.sh

--- gcloud/get_ip.sh
+++
@@ -1,3 +0,0 @@
-gcloud compute instances list --flatten networkInterfaces[].accessConfigs[] --format 'csv[no-heading](name,networkInterfaces.accessConfigs.natIP)' > name_ips.txt
-gcloud compute instances list --flatten networkInterfaces[].accessConfigs[] --format 'csv[no-heading](networkInterfaces.accessConfigs.natIP)' > ips.txt
-
