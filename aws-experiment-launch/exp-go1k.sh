76307a610965e276a6ed6c6552e405b55154abb3
ak <alok@harmony.one>
2018-09-18 16:40:19 -0700

    script and subsequent script

--- aws-experiment-launch/exp-go1k.sh
+++ aws-experiment-launch/exp-go1k.sh
@@ -6,6 +6,8 @@ SKIPCLIENT=false
 PARALLEL=500
 LAUNCH_OPT=
 DASHBOARD=
+#Leo: adding benchmark script here.
+RUN_BENCHMARK=run_benchmark-attack-cross90.sh 
 
 function usage
 {
@@ -57,25 +59,51 @@ mkdir -p logs
 
 SECONDS=0
 
+if [ "$SKIPCLIENT" == "false" ]; then
+   ./launch-client-only.sh r5.4xlarge &
+   LAUNCH_OPT+=' -i raw_ip-client.txt'
+fi
+
+if [ "$SKIPLEADER" == "false" ]; then
+   ./launch-leaders-only.sh &
+   LAUNCH_OPT+=' -l raw_ip-leaders.txt'
+fi
+
+wait
+
+# Leo: Launching the soldiers the first time. TOCHANGE: Currently 1200 nodes 150 per region 3 shards. Will change this for other exp. 
+./create_deploy_soldiers.sh -c 150 -s 3 -t 1 -m 0 -u configs/userdata-soldier-http.sh ${DASHBOARD} ${LAUNCH_OPT}
+
+if [ "$SKIPCLIENT" == "false" ]; then
+   cat instance_ids_output-client.txt >> instance_ids_output.txt
+   cat instance_output-client.txt >> instance_output.txt
+   rm instance_ids_output-client.txt instance_output-client.txt raw_ip-client.txt &
+fi
+
+
+if [ "$SKIPLEADER" == "false" ]; then
+   cat instance_ids_output-leaders.txt >> instance_ids_output.txt
+   cat instance_output-leaders.txt >> instance_output.txt
+   rm instance_ids_output-leaders.txt instance_output-leaders.txt &
+fi
 
 #sleep 10
-./run_benchmark.sh -n ${PARALLEL}  kill
-echo killing
-echo "starting"
+#./run_benchmark.sh ping
 
 sleep 3
 
-./run_benchmark-attack-cross60.sh -n ${PARALLEL} config
+./${RUN_BENCHMARK} -n ${PARALLEL} config
 
-echo "configed"
 sleep 3
 
 # enable dashboard
-./run_benchmark-attack-cross60.sh -n ${PARALLEL} -C -D 34.218.238.198:3000 init 
+# Leo: adding -C option for cross shard transactions.
+./${RUN_BENCHMARK} -n ${PARALLEL} -C -D 34.218.238.198:3000 init
 
 echo sleeping ...
-sleep 500
+sleep 300
 
+#Leo: Currently reading TS the old stile.
 pushd logs
 TS=$(ls -dlrt 2018* | tail -1 | awk -F ' ' ' { print $NF } ' )
 popd
@@ -93,14 +121,20 @@ sleep 3
 ./dl-soldier-logs.sh -s $TS -g leader soldier &
 ./dl-soldier-logs.sh -s $TS -g client benchmark &
 ./dl-soldier-logs.sh -s $TS -g client soldier &
-./dl-soldier-logs.sh -s $TS -p ${PARALLEL} -g validator benchmark &
 
+# Leo: Commenting out download of validator logs
+# ./dl-soldier-logs.sh -s $TS -p ${PARALLEL} -g validator benchmark &
+
+#Leo: killing current consensus.
+./${RUN_BENCHMARK} kill &
 # sleep 10
 
 wait
 
 aws s3 sync logs/$TS s3://harmony-benchmark/logs/$TS 2>&1 > /dev/null &
 aws s3 sync logs/run s3://harmony-benchmark/logs/run 2>&1 > /dev/null &
+
+# Leo not terminating instances now as we will use them for subsequent run
 #./terminate_instances.py 2>&1 > /dev/null &
 
 echo ============= TPS ==============
