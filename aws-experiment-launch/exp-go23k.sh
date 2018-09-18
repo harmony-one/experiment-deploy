887ed24263edb6f167183910208fec23a72e8423
EC2 Default User <ec2-user@ip-172-31-14-134.us-west-1.compute.internal>
2018-09-18 04:41:57 +0000

    Experimental scripts

---
+++ aws-experiment-launch/exp-go23k.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+
+THEPWD=$(pwd)
+SKIPLEADER=true
+SKIPCLIENT=false
+PARALLEL=500
+LAUNCH_OPT=
+DASHBOARD=
+
+function usage
+{
+   ME=$(basename $0)
+   cat<<EOT
+Usage: $ME [OPTIONS] ACTIONS
+
+This script automates the benchmark test.
+
+[OPTIONS]
+   -h             print this help message
+   -n num         launch num soldiers (default: $NUM)
+   -s             skip client launch (default: $SKIPCLIENT)
+   -S             skip power leader launch (default: $SKIPLEADER)
+   -i file        raw ip file of client (default: $IPFILE)
+   -d duration    duration of the benchmark test (default: $DURATION)
+   -D             enable dashboard (default: $DASHBOARD)
+
+[ACTIONS]
+   launch         do launch only
+   run            run benchmark
+   log            download logs
+   deinit         sync logs & terminate instances
+   all            do everything (default)
+
+
+[EXAMPLES]
+
+EOT
+   exit 0
+}
+
+
+while getopts "sShn:D" option; do
+   case $option in
+      S) SKIPLEADER=true ;;
+      s) SKIPCLIENT=true ;;
+      n) NUM=$OPTARG ;;
+      D) DASHBOARD='-D' ;;
+      h) usage ;;
+   esac
+done
+
+shift $(($OPTIND-1))
+
+ACTION=$@
+
+mkdir -p logs
+
+SECONDS=0
+
+
+#sleep 10
+./run_benchmark.sh kill
+echo killing
+sleep 300
+echo "starting"
+
+sleep 3
+
+./run_benchmark-attack-cross60.sh -n ${PARALLEL} config
+
+echo "configed"
+sleep 3
+
+# enable dashboard
+./run_benchmark-attack-cross60.sh -n ${PARALLEL} -C -D 34.218.238.198:3000 init 
+
+echo sleeping ...
+sleep 500
+
+pushd logs
+TS=$(ls -dlrt 2018* | tail -1 | awk -F ' ' ' { print $NF } ' )
+popd
+
+./dl-soldier-logs.sh -s $TS -g leader benchmark
+pushd logs/$TS/leader/tmp_log/log-$TS
+TPS=$( ${THEPWD}/cal_tps.sh )
+popd
+
+echo ============= TPS ==============
+echo $TPS
+
+sleep 3
+
+./dl-soldier-logs.sh -s $TS -g leader soldier &
+./dl-soldier-logs.sh -s $TS -g client benchmark &
+./dl-soldier-logs.sh -s $TS -g client soldier &
+./dl-soldier-logs.sh -s $TS -p ${PARALLEL} -g validator benchmark &
+
+# sleep 10
+
+wait
+
+aws s3 sync logs/$TS s3://harmony-benchmark/logs/$TS 2>&1 > /dev/null &
+aws s3 sync logs/run s3://harmony-benchmark/logs/run 2>&1 > /dev/null &
+#./terminate_instances.py 2>&1 > /dev/null &
+
+echo ============= TPS ==============
+echo $TPS
+
+wait
+echo ============= TPS ==============
+echo $TPS
+
+duration=$SECONDS
+
+echo This Run Takes $(($duration / 60)) minutes and $(($duration % 60)) seconds
---
+++ aws-experiment-launch/run_benchmark-attack-cross30.sh
@@ -0,0 +1,234 @@
+#!/usr/bin/env bash
+
+#TODO: parameter validation
+
+set -o pipefail
+
+if [ "$(uname -s)" == "Darwin" ]; then
+   TIMEOUT=gtimeout
+else
+   TIMEOUT=timeout
+fi
+
+
+function usage
+{
+   ME=$(basename $0)
+   cat<<EOT
+Usage: $ME [OPTIONS] ACTIONS
+
+OPTIONS:
+   -h                print this usage
+   -p profile        name of test profile (default: $PROFILE)
+   -f distribution   name of the distribution file (default: $DIST)
+   -i ip_addresses   ip addresses of the soldiers to run command
+                     delimited by ,
+   -a application    name of the application to be updated
+   -p testplan       file name of the test plan
+   -n num            parallel process num in a group (default: $PARALLEL)
+   -v                verbose
+   -D dashboard_ip   enable dashboard support, specify the ip address of dashboard server (default: $DASHBOARD)
+   -A                enable attack support, (default is on: $ATTACK)
+
+ACTIONS:
+   gen               generate test file based on profile (TODO)
+   auto              automate the test execution based on test plan (TODO)
+   ping              ping each soldier
+   config            configure distribution_config to each soldier
+   init              start benchmark test, after config
+   kill              kill all running benchmark/txgen 
+   update            update certain/all binaries on solider(s)
+
+EXAMPLES:
+
+   $ME -i 10.10.1.1,10.10.1,2 -a benchmark update
+   $ME -f dist1.txt -d myjason-format config
+EOT
+   exit 0
+}
+
+function read_profile
+{
+   if [ ! -f $PROFILE ]; then
+      echo ERROR: $PROFILE does not exist or not readable
+      exit 1
+   fi
+
+   BUCKET=$($JQ .bucket $PROFILE)
+   FOLDER=$($JQ .folder $PROFILE)
+   SESSION=$($JQ .sessionID $PROFILE)
+
+   LOGDIR=${CACHE}logs/$SESSION
+
+   mkdir -p $LOGDIR
+}
+
+function read_nodes
+{
+   read_profile
+
+   if [ ! -f $DIST ]; then
+      echo ERROR: $DIST does not exist or not readable
+      return 0
+   fi
+
+   n=1
+   while read line; do
+      fields=( $(echo $line) )
+      NODEIPS[$n]=${fields[0]}
+      NODES[${fields[0]}]=${fields[2]}
+      PORT[${fields[0]}]=${fields[1]}
+      (( n++ ))
+   done < $DIST
+
+   NUM_NODES=${#NODES[@]}
+   NUM_GROUPS=$(( $NUM_NODES / $PARALLEL ))
+   echo INFO: read ${NUM_NODES} nodes from $DIST
+}
+
+function do_simple_cmd
+{
+   local cmd=$1
+
+   mkdir -p $LOGDIR/$cmd
+
+   end=0
+   group=0
+   case $cmd in
+      config) cat>$LOGDIR/$cmd/$cmd.json<<EOT
+{
+   "sessionID":"$SESSION",
+   "configURL":"http://$BUCKET.s3.amazonaws.com/$FOLDER/distribution_config.txt"
+}
+EOT
+;;
+      init) cat>$LOGDIR/$cmd/$cmd.json<<EOT
+{
+   "ip":"127.0.0.1",
+   "port":"9000",
+   "benchmarkArgs":"$DASHBOARD $ATTACK $CROSSTX",
+   "txgenArgs":"-duration -1"
+}
+EOT
+;;
+      update)
+            if [ -z "$APP" ]; then
+               echo ERROR: no application name specified
+               exit 1
+            fi
+            cat>$LOGDIR/$cmd/$cmd.json<<EOT
+{
+   "bucket":"$BUCKET",
+   "folder":"$FOLDER",
+   "file":"$APP"
+}
+EOT
+;;
+   esac
+ 
+   SECONDS=0
+   while [ $end -lt $NUM_NODES ]; do
+      start=$(( $PARALLEL * $group + 1 ))
+      end=$(( $PARALLEL + $start - 1 ))
+
+      if [ $end -ge $NUM_NODES ]; then
+         end=$NUM_NODES
+      fi
+
+      echo processing group: $group \($start to $end\)
+
+      for n in $(seq $start $end); do
+         local ip=${NODEIPS[$n]}
+         CMD=$"curl -X GET -s http://$ip:1${PORT[$ip]}/$cmd -H \"Content-Type: application/json\""
+
+         case $cmd in
+            config|init|update)
+               CMD+=$" -d@$LOGDIR/$cmd/$cmd.json" ;;
+         esac
+
+         [ -n "$VERBOSE" ] && echo $n =\> $CMD
+         $TIMEOUT -s SIGINT 20s $CMD > $LOGDIR/$cmd/$cmd.$n.$ip.log &
+      done 
+      wait
+      (( group++ ))
+   done
+   duration=$SECONDS
+
+   succeeded=$(find $LOGDIR/$cmd -name $cmd.*.log -type f -exec grep Succeeded {} \; | wc -l)
+   failed=$(( $NUM_NODES - $succeeded ))
+
+   echo $(date): $cmd succeeded/$succeeded, failed/$failed nodes, $(($duration / 60)) minutes and $(($duration % 60)) seconds
+
+   rm -rf $LOGDIR/$cmd
+}
+
+function do_update
+{
+   if [ -z "$APP" ]; then
+      echo ERROR: no application name specified
+      exit 1
+   fi
+
+   succeeded=0
+   failed=0
+   date
+   for n in "${!NODES[@]}"; do
+      res=$(curl -s -XGET http://$n:1${PORT[$n]}/update \
+      --header "content-type: application/json" \
+      -d "{\"bucket\":\"$BUCKET\",\"folder\":\"$FOLDER\",\"file\":\"$APP\"}" )
+
+      if [ "$res" == "Succeeded" ]; then
+         (( succeeded++ ))
+      else
+         echo ERROR: $res
+         (( failed++ ))
+      fi
+   done
+
+   echo $(date): Update succeeded/$succeeded, failed/$failed nodes
+}
+
+function generate_tests
+{
+   echo todo
+}
+
+#################### VARS ####################
+JQ='jq -r -M'
+PROFILE=configs/profile.json
+DIST=distribution_config.txt
+PARALLEL=100
+VERBOSE=
+DASHBOARD=
+ATTACK="-attacked_mode 2"
+
+declare -A NODES
+declare -A NODEIPS
+declare -A PORT
+
+#################### MAIN ####################
+while getopts "hp:f:i:a:n:vD:AC" option; do
+   case $option in
+      p) PROFILE=$OPTARG ;;
+      f) DIST=$OPTARG ;;
+      i) IPS=$OPTARG ;;
+      a) APP=$OPTARG ;;
+      n) PARALLEL=$OPTARG ;;
+      v) VERBOSE=true ;;
+      D) DASHBOARD="-metrics_report_url http://$OPTARG/report" ;;
+      A) ATTACK="-attacked_mode 2" ;;
+      C) CROSSTX="-cross_shard_ratio 30" ;;
+      h|?) usage ;;
+   esac
+done
+
+shift $(($OPTIND-1))
+
+ACTION=$@
+
+case "$ACTION" in
+   "gen") read_nodes; generate_tests ;;
+   "ping"|"kill"|"config"|"init"|"update") read_nodes; do_simple_cmd $ACTION ;;
+   "auto") read_nodes; do_auto ;;
+   *) usage ;;
+esac
