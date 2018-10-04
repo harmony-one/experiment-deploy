0611395093d46cc404a5d475aaf6dab4456a8e10
Leo Chen <leo@harmony.one>
2018-10-04 05:11:35 +0000

    clean up some legacy code in spot.go
    add all region configuration in aws.json
    
    Signed-off-by: Leo Chen <leo@harmony.one>

--- aws/spot/spot.go
+++ aws/spot/spot.go
@@ -6,15 +6,10 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 
-	//"github.com/aws/aws-sdk-go/service/cloudwatch"
-
 	//	"math"
-	//	"crypto/rsa"
-	//	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
-	//	"encoding/pem"
 	"flag"
 	"fmt"
 	"github.com/kr/pretty"
@@ -193,38 +188,15 @@ func launchSpotInstances(reg *Region, i *InstanceConfig) error {
 		return fmt.Errorf("findAMI Error %v", err)
 	}
 
-	/*
-		subnets, err := findSubnet(svc, reg.Vpc)
-		if err != nil {
-			fmt.Printf("findSubnet Error %v", err)
-			return fmt.Errorf("findSubnet Error %v", err)
-		}
-		numSubnets := len(subnets)
-		numInstances := int(math.Round(float64(i.Number / numSubnets)))
-		if numInstances == 0 {
-			numInstances = 1
-		}
-		remains := i.Number
-	*/
-	numInstances := i.Number
 	var reservations []*string
-	/*	for n := 0; remains > 0; remains = remains - numInstances {
-		if n == numSubnets-1 {
-			numInstances = remains
-		}
-		subnet := subnets[n]
-		n = n + 1
-	*/
 	input := ec2.RunInstancesInput{
 		ImageId:          aws.String(amiId),
 		InstanceType:     aws.String(i.Type),
-		MinCount:         aws.Int64(int64(numInstances)),
-		MaxCount:         aws.Int64(int64(numInstances)),
+		MinCount:         aws.Int64(int64(i.Number)),
+		MaxCount:         aws.Int64(int64(i.Number)),
 		KeyName:          aws.String(reg.KeyPair),
 		SecurityGroupIds: []*string{aws.String(reg.Vpc.Sg)},
 	}
-
-	//			SubnetId:         aws.String(*subnet.SubnetId)
 
 	if *debug > 1 {
 		fmt.Printf("input: %v\n", pretty.Formatter(input))
@@ -241,8 +213,7 @@ func launchSpotInstances(reg *Region, i *InstanceConfig) error {
 	}
 
 	reservations = append(reservations, reservation.ReservationId)
-	fmt.Printf("Reservations: %v", reservations)
-	//	}
+	fmt.Printf("Reservations: %v\n", reservations)
 
 	instanceInput := &ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
@@ -253,7 +224,7 @@ func launchSpotInstances(reg *Region, i *InstanceConfig) error {
 		},
 	}
 
-	fmt.Printf("sleeping for %d seconds", WAIT_COUNT)
+	fmt.Printf("sleeping for %d seconds\n", WAIT_COUNT)
 	time.Sleep(WAIT_COUNT * time.Second)
 
 	for m := 0; m < WAIT_COUNT; m++ {
@@ -323,8 +294,6 @@ func main() {
 		fmt.Printf("launch: %# v\n", pretty.Formatter(launches))
 	}
 
-	//regions := strings.Split(*regionList, ",")
-
 	var userDataString string
 	if data, err := ioutil.ReadFile(launches.UserData.File); err != nil {
 		exitErrorf("Unable to read userdata file: %v", launches.UserData.File)
@@ -351,19 +320,4 @@ func main() {
 			exitErrorf("launchSpotInstances Error: %v\n", err)
 		}
 	}
-
-	/*
-		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				switch aerr.Code() {
-				case "InvalidGroupId.Malformed":
-					fallthrough
-				case "InvalidGroup.NotFound":
-					exitErrorf("%s.", aerr.Message())
-				}
-			}
-			exitErrorf("Launch Instance Failure, %v", err)
-		}
-	*/
-
 }
