90d06b5082fe7fc62e3ebeb58677e2fb726bb039
Andy Wu <bwu2sfu@gmail.com>
2019-06-01 11:46:17 -0700

    removed Mac implementation, refactor the code per LC's comments

--- configs/userdata-soldier-http.sh
+++ configs/userdata-soldier-http.sh
@@ -110,22 +110,14 @@ setup_node_exporter() {
 
 	# node_exporter version: 0.18.0/2019-05-09
 	URL_node_exporter_linux=https://github.com/prometheus/node_exporter/releases/download/v0.18.0/node_exporter-0.18.0.linux-amd64.tar.gz
-	URL_node_exporter_mac=https://github.com/prometheus/node_exporter/releases/download/v0.18.0/node_exporter-0.18.0.darwin-amd64.tar.gz
 
+  # download and decompress the node exporter in the tmp folder
 	cd /tmp 
-	# download different version for Mac and Linux, and
-	# need to create a service user to deploy node_exporter as a service
-	if [ "$OS" == "Darwin" ]; then
-		curl -LO $URL_node_exporter_mac
-		#[TO-DO] add a service user in mac
-	fi
-	if [ "$OS" == "Linux" ]; then
-		curl -LO $URL_node_exporter_linux
-		useradd -rs /bin/false node_exporter
-	fi
-
-	# decompress the file
-	tar -xvf /tmp/node_exporter-0.18.0.$os-amd64.tar.gz
+	curl -LO $URL_node_exporter_linux
+  tar -xvf /tmp/node_exporter-0.18.0.$os-amd64.tar.gz
+  # add a servcie account for node_exporter
+	useradd -rs /bin/false node_exporter
+	
 
 	# move the node export binary to /usr/local/bin
 	mv /tmp/node_exporter-0.18.0.$os-amd64/node_exporter /usr/local/bin/
@@ -153,7 +145,7 @@ setup_node_exporter() {
 	systemctl daemon-reload
 	systemctl start node_exporter
 
-	enable the node exporter servie to the system startup
+	#enable the node exporter servie to the system startup
 	systemctl enable node_exporter
 
 }
