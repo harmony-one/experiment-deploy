13bcad1924d0bfa0ba0af002e45b9ec91fa4ac22
Eugene Kim <ek@harmony.one>
2019-06-22 23:22:42 +0000

    Add -U (upgrade), -B (bucket), -F (folder) flags

--- pipeline/restart_node.sh
+++ pipeline/restart_node.sh
@@ -15,10 +15,12 @@ esac
 . "${progdir}/util.sh"
 . "${progdir}/common_opts.sh"
 
-unset -v default_timeout default_step_retries default_cycle_retries
-default_timeout=90
+unset -v default_timeout default_step_retries default_cycle_retries default_bucket
+default_timeout=300
 default_step_retries=2
 default_cycle_retries=2
+default_bucket=unique-bucket-bin
+default_folder="${WHOAMI}"
 
 print_usage() {
 	cat <<- ENDEND
@@ -39,16 +41,22 @@ print_usage() {
 		-t N		wait at most N seconds for BINGO/HOORAY (default: ${default_timeout})
 		-r N		try a failed step N more times (default: ${default_step_retries})
 		-R N		try a failed cycle N more times (default: ${default_cycle_retries})
+		-U		upgrade the node software
+		-B BUCKET	fetch upgrade binaries from the given bucket
+		 		(default: ${default_bucket})
+		-F FOLDER	fetch upgrade binaries from the given folder
+		 		(default: ${default_folder})
 
 		arguments:
 		ip		the IP address to upgrade
 	ENDEND
 }
 
-unset -v timeout step_retries cycle_retries
+unset -v timeout step_retries cycle_retries upgrade bucket folder
+upgrade=false
 unset -v OPTIND OPTARG opt
 OPTIND=1
-while getopts ":${common_getopts_spec}t:r:R:" opt
+while getopts ":${common_getopts_spec}t:r:R:UB:F:" opt
 do
 	! process_common_opts "${opt}" || continue
 	case "${opt}" in
@@ -57,6 +65,9 @@ do
 	t) timeout="${OPTARG}";;
 	r) step_retries="${OPTARG}";;
 	R) cycle_retries="${OPTARG}";;
+	U) upgrade=true;;
+	B) bucket="${OPTARG}";;
+	F) folder="${OPTARG}";;
 	*) err 70 "unhandled option -${OPTARG}";;
 	esac
 done
@@ -65,6 +76,8 @@ shift $((${OPTIND} - 1))
 : ${timeout="${default_timeout}"}
 : ${step_retries="${default_step_retries}"}
 : ${cycle_retries="${default_cycle_retries}"}
+: ${bucket="${default_bucket}"}
+: ${folder="${default_folder}"}
 
 case "${timeout}" in
 ""|*[^0-9]*) usage "invalid timeout: ${timeout}";;
@@ -141,6 +154,17 @@ get_logfile() {
 run_with_retries get_logfile
 rn_info "log file is ${logfile}"
 
+unset -v s3_folder
+s3_folder="s3://${bucket}/${folder}"
+rn_debug "s3_folder=$(shell_quote "${s3_folder}")"
+
+fetch_binaries() {
+	"${progdir}/node_ssh.sh" -d "${logdir}" "${ip}" "
+		aws s3 sync $(shell_quote "${s3_folder}") staging
+		ls -l staging
+	"
+}
+
 kill_harmony() {
 	local soldier_status status
 	if soldier_status=$(curl -s "http://${ip}:19000/kill")
@@ -192,6 +216,23 @@ wait_for_harmony_process_to_exit() {
 		sleep=$((${sleep} + 1))
 		sleep ${sleep}
 	done
+}
+
+upgrade_binaries() {
+	"${progdir}/node_ssh.sh" -d "${logdir}" "${ip}" '
+		set -eu
+		unset -v f
+		for f in harmony txgen wallet libmcl.so libbls384_256.so
+		do
+			rm -f "${f}"
+			cp -p "staging/${f}" "${f}"
+			case "${f}" in
+			harmony|txgen|wallet)
+				chmod a+x "${f}"
+				;;
+			esac
+		done
+	'
 }
 
 unset -v logsize
@@ -258,10 +299,20 @@ do
 	while :
 	do
 		cycle_ok=false
+		if ${upgrade}
+		then
+			rn_info "fetching upgrade binaries"
+			run_with_retries fetch_binaries || break
+		fi
 		rn_info "killing harmony process"
 		run_with_retries kill_harmony || break
 		rn_info "waiting for harmony process to exit (if any)"
 		run_with_retries wait_for_harmony_process_to_exit || break
+		if ${upgrade}
+		then
+			rn_info "upgrading node software"
+			run_with_retries upgrade_binaries || break
+		fi
 		rn_info "getting logfile size"
 		run_with_retries get_logfile_size || break
 		rn_info "log file is ${logsize} bytes"
