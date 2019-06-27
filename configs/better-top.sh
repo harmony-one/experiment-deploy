6a9e91b6dfe3b246d90048ee845f2093f030c4ce
Andy Wu <bwu2sfu@gmail.com>
2019-06-26 22:28:12 -0700

    fixed new line issue

--- configs/better-top.sh
+++ configs/better-top.sh
@@ -4,4 +4,5 @@ while read -r z
 do
    var=$var$(awk '{print "cpu_usage{process=\""$11"\", pid=\""$2"\"}", $3z}');
 done <<< "$z"
-curl -X POST -H  "Content-Type: text/plain" --data "$var" http://localhost:9091/metrics/job/top/instance/machine
+curl -X POST -H  "Content-Type: text/plain" --data "$var
+" http://localhost:9091/metrics/job/top/instance/machine
