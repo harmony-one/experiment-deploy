a54c0cbe37ad20ee75944bd491284e76766d00fd
Andy Wu <bwu2sfu@gmail.com>
2019-07-04 20:52:45 -0700

    make harmony running as a service

--- configs/userdata-soldier-http.sh
+++ configs/userdata-soldier-http.sh
@@ -247,7 +247,41 @@ setup_better_top() {
 
 }
 
+systemd_harmony () {
+   
+   set -eu
 
+   pushd /home/ec2-user
+
+   harmony_service=/etc/systemd/system/harmony.service
+
+   useradd -rs /bin/false harmony
+
+   echo "[*] creating harmony service file .. "
+   echo "
+[Unit]
+Description=harmony service
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=harmony
+ExecStart=/home/ec2-user/node.sh -1
+
+[Install]
+WantedBy=multi-user.target" > $harmony_service
+
+   systemctl daemon-reload
+   systemctl start harmony
+
+   systemctl enable harmony
+
+   popd
+
+}
 
 
 function restore_db {
@@ -322,6 +356,9 @@ setup_pushgateway
 
 # deploy better-top.sh as a service
 setup_better_top
+
+
+systemd_harmony
 
 
 # Run soldier
