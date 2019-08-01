25a09141fa727528f647e0d851099d6e1168d324
Leo Chen <leo@harmony.one>
2019-07-31 22:21:58 -0700

    [tf] init commit of terraform script
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ tf/aws/sg.tf
@@ -0,0 +1,51 @@
+# Security Group with Harmoney Node ports enabled
+
+resource "aws_security_group" "harmony-node-sg" {
+  name        = "Harmony Node Security Group"
+  description = "Security group for Harmony Nodes"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["24.6.223.94/32"]
+    description = "Enable SSH from MTV office wifi"
+  }
+
+  ingress {
+    from_port   = 6000
+    to_port     = 6000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Harmony Node State Syncing Port"
+  }
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Harmony Basic Port"
+  }
+
+  ingress {
+    from_port   = 14555
+    to_port     = 14555
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Harmony RPC Port"
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "Harmony Node Security Group"
+    Project = "Harmony"
+  }
+}
