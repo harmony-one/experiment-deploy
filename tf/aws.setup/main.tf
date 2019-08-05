33fa35f5bf4e4995d24a68e3f02ac744e5713c11
Leo Chen <leo@harmony.one>
2019-08-05 00:01:00 -0700

    [tf] add aws setup script
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ tf/aws.setup/main.tf
@@ -0,0 +1,69 @@
+module "init-us-east-1" {
+  source = "./setup"
+  region = "us-east-1"
+}
+
+module "init-us-east-2" {
+  source = "./setup"
+  region = "us-east-2"
+}
+
+module "init-us-west-1" {
+  source = "./setup"
+  region = "us-west-1"
+}
+
+module "init-us-west-2" {
+  source = "./setup"
+  region = "us-west-2"
+}
+
+module "init-ca-central-1" {
+  source = "./setup"
+  region = "ca-central-1"
+}
+
+module "init-eu-west-1" {
+  source = "./setup"
+  region = "eu-west-1"
+}
+
+module "init-eu-west-2" {
+  source = "./setup"
+  region = "eu-west-2"
+}
+
+module "init-eu-central-1" {
+  source = "./setup"
+  region = "eu-central-1"
+}
+
+module "init-ap-northeast-1" {
+  source = "./setup"
+  region = "ap-northeast-1"
+}
+
+module "init-ap-northeast-2" {
+  source = "./setup"
+  region = "ap-northeast-2"
+}
+
+module "init-ap-southeast-1" {
+  source = "./setup"
+  region = "ap-southeast-1"
+}
+
+module "init-ap-southeast-2" {
+  source = "./setup"
+  region = "ap-southeast-2"
+}
+
+module "init-ap-south-1" {
+  source = "./setup"
+  region = "ap-south-1"
+}
+
+module "init-sa-east-1" {
+  source = "./setup"
+  region = "sa-east-1"
+}
---
+++ tf/aws.setup/setup/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {}
+
+variable "public_key_path" {
+  description = "The path to the SSH Public Key to add to AWS."
+  default     = "~/.ssh/harmony-node.pub"
+}
+
+variable "private_key_path" {
+  description = "The path to the SSH Private Key to access ec2 instance."
+  default     = "~/.ssh/harmony-node.pem"
+}
+
+variable "node_owner" {
+  description = "The user starts the node instance"
+  default     = "LeoChen"
+}
