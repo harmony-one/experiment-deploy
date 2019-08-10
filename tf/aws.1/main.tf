40c560a05e37028b50ce7a681d86898e89ca926c
Leo Chen <leo@harmony.one>
2019-08-09 18:22:59 -0700

    [tf] update userdata.sh to install jq
    Signed-off-by: Leo Chen <leo@harmony.one>

--- tf/aws.1/main.tf
+++ tf/aws.1/main.tf
@@ -33,4 +33,57 @@ resource "aws_spot_instance_request" "foundation-node" {
    command = "aws s3 cp s3://harmony-secret-keys/bls/${lookup(var.harmony-nodes-blskeys, var.blskey_index, var.default_key)}.key files/bls.key"
    }
 
+  provisioner "file" {
+    source      = "files/bls.key"
+    destination = "/home/ec2-user/bls.key"
+    connection {
+      host        = "${aws_spot_instance_request.foundation-node.public_ip}"
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.private_key_path)}"
+      agent       = true
+    }
+  }
+
+  provisioner "file" {
+    source      = "files/bls.pass"
+    destination = "/home/ec2-user/bls.pass"
+    connection {
+      host        = "${aws_spot_instance_request.foundation-node.public_ip}"
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.private_key_path)}"
+      agent       = true
+    }
+  }
+
+  provisioner "file" {
+    source      = "files/harmony.service"
+    destination = "/home/ec2-user/harmony.service"
+    connection {
+      host        = "${aws_spot_instance_request.foundation-node.public_ip}"
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.private_key_path)}"
+      agent       = true
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "curl -LO https://harmony.one/node.sh",
+      "chmod +x node.sh",
+      "sudo mv -f harmony.service /etc/systemd/system/harmony.service",
+      "sudo systemctl enable harmony.service",
+      "sudo systemctl start harmony.service",
+    ]
+    connection {
+      host        = "${aws_spot_instance_request.foundation-node.public_ip}"
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.private_key_path)}"
+      agent       = true
+    }
+  }
+
 }
