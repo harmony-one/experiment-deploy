260a6355f76b70e8465894c72e93becb18ef01b6
Leo Chen <leo@harmony.one>
2020-01-26 23:06:08 -0800

    [tf] add wait/uptime for aws script
    Signed-off-by: Leo Chen <leo@harmony.one>

--- tf/aws.1/go.sh
+++ tf/aws.1/go.sh
@@ -3,6 +3,7 @@
 
 source ./regions.sh
 ME=`basename $0`
+SSH='ssh -o StrictHostKeyChecking=no -o LogLevel=error -o ConnectTimeout=5 -o GlobalKnownHostsFile=/dev/null'
 
 set -o pipefail
 set -x
@@ -54,7 +55,7 @@ function usage
 {
    cat<<EOF
 Usage: $ME [Options] Command
-This script will be used manage terraform nodes: launch new , replace existing.
+This script will be used to manage AWS terraform nodes: launch new one, run rclone, wait for rclone finish
 
 PREREQUISITE:
  * this script can only be run on devop.hmny.io host
@@ -73,10 +74,10 @@ OPTIONS:
    -d log-file-directory      specify the directory of the log directory (default: logs/$HMY_PROFILE)
 
 COMMANDS:
-   replace [list of ip]       list of IP addresses of the existing node to be replaced, delimited by space
    new [list of index]        list of index of the harmony node in internal/genesis/harmony.go, delimited by space
-   fast [list of ip]          list of IP addresses to do fast state syncing using snapshot, delimited by space
    rclone [list of ip]        list of IP addresses to do rlcone, delimited by space
+   wait [list of ip]          list of IP addresses to wait until rclone finished, check every minute
+   uptime [list of ip]        list of IP addresses to update uptime robot, will generate uptimerobot.sh cli
 
 EXAMPLES:
 
@@ -84,9 +85,9 @@ EXAMPLES:
 
    $ME new 20 30 5
 
-   $ME replace 12.34.56.78 123.234.123.234
+   $ME rclone 12.34.56.78 123.234.123.234
 
-   $ME fast 12.34.56.78 123.234.123.234
+   $ME wait 12.34.56.78 123.234.123.234
 
 EOF
    exit 0
@@ -122,64 +123,13 @@ function do_state_sync
 function new_instance
 {
    indexes=$@
+   rm -f ip.txt
    for i in $indexes; do
       _do_launch_one $i
+      echo $IP >> ip.txt
    done
 
    do_state_sync
-}
-
-# find index number based on the state file
-function _find_index_from_state
-{
-   ip=$1
-   index=$(grep -l $ip $STATEDIR/terraform.tfstate.* | awk -F. ' { print $3 } ')
-   echo $index
-}
-
-# find index number based on the init file
-function _find_index_from_init
-{
-   ip=$1
-   file=$(ls $LOGDIR/init/*init-$ip.json)
-   if [ -f "$file" ]; then
-      key=$(grep -oE 'blskey_file .*.key' $file | awk ' { print $2 } ' | sed 's/.key//')
-      if [ -n "$key" ]; then
-         index=$(grep $key variables.tf | awk ' { print $1 } ' | tr -d \")
-         echo $index
-      fi
-   fi
-}
-
-# replace existing instance
-function replace_instance
-{
-   ips=$@
-   for ip in $ips; do
-      index=$(_find_index_from_state $ip)
-      if [ -z "$index" ]; then
-         index=$(_find_index_from_init $ip)
-      fi
-      if [ -n "$index" ]; then
-         echo "ready to launch new instance with index: $index (yn)?"
-         read yesno
-         if [[ "$yesno" == "y" || "$yesno" == "Y" ]]; then
-            _do_launch_one $index
-         else
-            return
-         fi
-      fi
-   done
-   do_state_sync
-}
-
-# do fast state syncing using the db snapshot
-function fast_sync
-{
-   ips=$@
-   for ip in $ips; do
-      ssh ec2-user@$ip 'nohup /home/ec2-user/fast.sh > fast.log 2> fast.err < /dev/null &'
-   done
 }
 
 # use rclone to sync harmony db
@@ -187,7 +137,67 @@ function rclone_sync
 {
    ips=$@
    for ip in $ips; do
-      ssh ec2-user@$ip 'nohup /home/ec2-user/rclone.sh > rclone.log 2> rclone.err < /dev/null &'
+      $SSH ec2-user@$ip 'nohup /home/ec2-user/rclone.sh > rclone.log 2> rclone.err < /dev/null &'
+   done
+}
+
+function do_wait
+{
+   ips=$@
+   declare -A DONE
+   for ip in $ips; do
+      DONE[$ip]=false
+   done
+
+   min=0
+   while true; do
+      for ip in $ips; do
+         rc=$($SSH ec2-user@$ip 'pgrep -n rclone')
+         if [ -n "$rc" ]; then
+            echo "rclone is running on $ip. pid: $rc."
+         else
+            echo "rclone is not running on $ip."
+         fi
+         hmy=$($SSH ec2-user@$ip 'pgrep -n harmony')
+         if [ -n "$hmy" ]; then
+            echo "harmony is running on $ip. pid: $hmy"
+            DONE[$ip]=true
+         else
+            echo "harmony is not running on $ip."
+         fi
+      done
+
+      alldone=true
+      for ip in $ips; do
+         if ! ${DONE[$ip]}; then
+            alldone=false
+            break
+         fi
+      done
+
+      if $alldone; then
+         echo All Done!
+         break
+      fi
+      echo "sleeping 60s, $min minutes passed"
+      sleep 60
+      (( min++ ))
+   done
+
+   for ip in $ips; do
+      $SSH ec2-user@$ip 'tac latest/zerolog*.log | grep -m 1 BINGO'
+   done
+   date
+}
+
+function update_uptime
+{
+   ips=$@
+   for ip in $ips; do
+      idx=$($SSH ec2-user@$ip 'cat index.txt')
+      shard=$(( $idx % 4 ))
+      echo ./uptimerobot.sh -t t3 -i $idx update t3-$idx $ip $shard
+      echo ./uptimerobot.sh -t t3 -i $idx -G update t3-$idx $ip $shard
    done
 }
 
@@ -228,8 +238,6 @@ fi
 
 case $CMD in
    new) new_instance $@ ;;
-   replace) replace_instance $@ ;;
-   fast) fast_sync $@ ;;
    rclone) rclone_sync $@ ;;
    *) usage ;;
 esac
