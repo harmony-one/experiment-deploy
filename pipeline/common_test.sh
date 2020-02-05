a1c021483b3c72a7841f3f3d4465aba0f455149f
Leo Chen <leo@harmony.one>
2020-02-05 03:56:38 +0000

    [common] support multikey configuration
    add function to generate multi-key index and unit test
    
    Signed-off-by: Leo Chen <leo@harmony.one>

--- pipeline/common_test.sh
+++ pipeline/common_test.sh
@@ -56,5 +56,31 @@ function test_find_available_node_index
 
 # test_read_blskey
 
-test_read_profile
+# test_read_profile
 # test_find_available_node_index
+
+function test_gen_multi_key
+{
+   keys=$(gen_multi_key 2 10 2 0 0)
+   if [ "$keys" != "0 10" ]; then
+      echo failed, got "'$keys'", expect "'0 10'"
+   fi
+   keys=$(gen_multi_key 2 10 2 0 1)
+   if [ "$keys" != "2 12" ]; then
+      echo failed, got "'$keys'", expect "'2 12'"
+   fi
+   keys=$(gen_multi_key 2 10 3 0 3)
+   if [ "$keys" != "18" ]; then
+      echo failed, got "'$keys'", expect "'18'"
+   fi
+   keys=$(gen_multi_key 2 10 3 1 3)
+   if [ "$keys" != "19" ]; then
+      echo failed, got "'$keys'", expect "'19'"
+   fi
+   keys=$(gen_multi_key 3 11 3 1 3)
+   if [ "$keys" != "28 31" ]; then
+      echo failed, got "'$keys'", expect "'28 31'"
+   fi
+}
+
+test_gen_multi_key
