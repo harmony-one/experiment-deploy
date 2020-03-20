5cafdf51293e40283d436631743517a97352b9c7
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-03-20 15:52:34 -0700

    [pipeline] Update fund.py and fund.sh to always pull from spreadsheet
    * Update nonce setting to account for different balances

--- pipeline/fund.py
+++ pipeline/fund.py
@@ -13,6 +13,7 @@ import random
 import time
 import argparse
 import os
+import csv
 from multiprocessing.pool import ThreadPool
 from decimal import Decimal
 
@@ -74,6 +75,10 @@ def parse_args():
     parser.add_argument("--check", action="store_true", help="Spot check balances after funding")
     parser.add_argument("--force", action="store_true", help="Send transactions even if network appears to be offline")
     parser.add_argument("--yes", action="store_true", help="Say yes to profile check")
+    parser.add_argument("--from_csv", dest="csv", help="Path to CSV file of keys, i.e: (harmony.one/keys2). "
+                                                       "Note the file format assumption. "
+                                                       "If given, the `amount` and `accounts` options are ignored. ",
+                        default=None)
     p_arg = parser.parse_args()
     p_arg.accounts = accounts if p_arg.accounts is None else [el.strip()
                                                               for el in p_arg.accounts.split(",")
@@ -101,7 +106,7 @@ def get_balance(address, endpoint):
     }
     response = requests.request('POST', url, headers=headers, data=payload, allow_redirects=False, timeout=30)
     atto_bal = int(json.loads(response.content)["result"], 16)
-    return float(Decimal(atto_bal)/Decimal(1e18))
+    return float(Decimal(atto_bal) / Decimal(1e18))
 
 
 def get_nonce(endpoint, address):
@@ -152,35 +157,97 @@ def get_endpoints(config):
     return eps
 
 
-def fund(shard):
+def fund(shard, data=None):
+    if not data:
+        return fund_from_param(shard)
+    return fund_from_csv_data(shard, data)
+
+
+def fund_from_param(shard):
     if shard >= len(endpoints):
         return
     transactions = []
-    starting_nonce = get_nonce(endpoints[shard], faucet_addr)
+    nonce = get_nonce(endpoints[shard], faucet_addr)
     print(f"{util.Typgpy.HEADER}Preparing transactions for shard {shard} "
           f"({len(args.accounts)} transaction(s)){util.Typgpy.ENDC}")
-    for j, acc in enumerate(args.accounts):
-        if get_balance(acc, endpoints[shard]) == 0:
+    for acc in args.accounts:
+        fund_amount = float(args.amount) - get_balance(acc, endpoints[shard])  # WARNING: Always fund the difference.
+        if fund_amount > 0:
             transactions.append({
                 "from": faucet_addr,
                 "to": acc,
                 "from-shard": str(shard),
                 "to-shard": str(shard),
                 "passphrase-string": "",
-                "amount": str(args.amount),
-                "nonce": str(starting_nonce + j),
+                "amount": str(fund_amount),
+                "nonce": str(nonce),
             })
+            nonce += 1
     filename = f"./fund-{os.environ['HMY_PROFILE']}.s{shard}.json"
     with open(filename, 'w') as f:
         json.dump(transactions, f, indent=4)
     command = f"hmy --node={endpoints[shard]} transfer --file {filename} --chain-id {chain_id} --timeout 0"
     if transactions:
         print(f"{util.Typgpy.HEADER}Sending {len(transactions)} transaction(s) on shard {shard}!{util.Typgpy.ENDC}")
-        print(f"{util.Typgpy.HEADER}Transaction for shard {shard}:\n{util.Typgpy.OKGREEN}"
-              f"{cli.single_call(command, timeout=int(args.timeout) * len(endpoints) * len(args.accounts))} "
-              f"{util.Typgpy.ENDC}")
+        try:
+            print(f"{util.Typgpy.HEADER}Transaction for shard {shard}:\n{util.Typgpy.OKGREEN}"
+                  f"{cli.single_call(command, timeout=int(args.timeout) * len(endpoints) * len(args.accounts))} "
+                  f"{util.Typgpy.ENDC}")
+        except Exception as e:
+            print(f"{util.Typgpy.FAIL}Transaction error: {e}{util.Typgpy.ENDC}")
     else:
         print(f"{util.Typgpy.FAIL}No transactions to send on shard {shard}!{util.Typgpy.ENDC}")
+
+
+# TODO: generalize funding code to remove code duplication
+def fund_from_csv_data(shard, data):
+    if shard >= len(endpoints):
+        return
+    transactions = []
+    nonce = get_nonce(endpoints[shard], faucet_addr)
+    print(f"{util.Typgpy.HEADER}Preparing transactions for shard {shard} "
+          f"({len(args.accounts)} transaction(s)){util.Typgpy.ENDC}")
+    for dat in data:
+        fund_amount = float(dat['amount']) - get_balance(dat['address'], endpoints[shard])  # WARNING: Always fund the difference.
+        if fund_amount > 0:
+            transactions.append({
+                "from": faucet_addr,
+                "to": dat['address'],
+                "from-shard": str(shard),
+                "to-shard": str(shard),
+                "passphrase-string": "",
+                "amount": str(fund_amount),
+                "nonce": str(nonce),
+            })
+            nonce += 1
+    filename = f"./fund-{os.environ['HMY_PROFILE']}.s{shard}.json"
+    with open(filename, 'w') as f:
+        json.dump(transactions, f, indent=4)
+    command = f"hmy --node={endpoints[shard]} transfer --file {filename} --chain-id {chain_id} --timeout 0"
+    if transactions:
+        print(f"{util.Typgpy.HEADER}Sending {len(transactions)} transaction(s) on shard {shard}!{util.Typgpy.ENDC}")
+        try:
+            print(f"{util.Typgpy.HEADER}Transaction for shard {shard}:\n{util.Typgpy.OKGREEN}"
+                  f"{cli.single_call(command, timeout=int(args.timeout) * len(endpoints) * len(args.accounts))} "
+                  f"{util.Typgpy.ENDC}")
+        except Exception as e:
+            print(f"{util.Typgpy.FAIL}Transaction error: {e}{util.Typgpy.ENDC}")
+    else:
+        print(f"{util.Typgpy.FAIL}No transactions to send on shard {shard}!{util.Typgpy.ENDC}")
+
+
+def parse_csv():
+    data = []
+    if args.csv is not None:
+        with open(args.csv, 'r') as f:
+            for row in csv.DictReader(f):
+                raw_amount, raw_address = row['funded'], row['validator address']  # WARNING: Assumption of column name of CSV
+                if raw_amount and 'one1' in raw_address:
+                    data.append({
+                        'amount': float(raw_amount.strip().replace(',', '')),
+                        'address': raw_address.strip()
+                    })
+    return data
 
 
 def get_balance_from_node_ip(address, endpoint_list):
@@ -214,12 +281,17 @@ if __name__ == "__main__":
     if os.environ['HMY_PROFILE'] is None:
         raise RuntimeError(f"{util.Typgpy.FAIL}Profile is not set, exiting...{util.Typgpy.ENDC}")
 
+    csv_data = parse_csv()
     print(f"{util.Typgpy.HEADER}Funding using endpoints: {util.Typgpy.OKGREEN}{endpoints}{util.Typgpy.ENDC}")
     print(f"{util.Typgpy.HEADER}Funding on shards: {util.Typgpy.OKGREEN}{args.shards}{util.Typgpy.ENDC}")
-    print(f"{util.Typgpy.HEADER}Amount to fund per shard: {util.Typgpy.OKGREEN}{args.amount}{util.Typgpy.ENDC}")
-    print(f"{util.Typgpy.HEADER}Count of accounts to fund: {util.Typgpy.OKGREEN}{len(args.accounts)}{util.Typgpy.ENDC}")
     print(f"{util.Typgpy.HEADER}Chain-ID: {util.Typgpy.OKGREEN}{chain_id}{util.Typgpy.ENDC}")
     print(f"{util.Typgpy.HEADER}Profile: {util.Typgpy.OKGREEN}{os.environ['HMY_PROFILE']}{util.Typgpy.ENDC}")
+    if csv_data:
+        print(f"{util.Typgpy.HEADER}Funding from CSV: {util.Typgpy.OKGREEN}{args.csv}{util.Typgpy.ENDC}")
+    else:
+        print(f"{util.Typgpy.HEADER}Amount to fund per shard: {util.Typgpy.OKGREEN}{args.amount}{util.Typgpy.ENDC}")
+        print(f"{util.Typgpy.HEADER}Count of accounts to fund: {util.Typgpy.OKGREEN}"
+              f"{len(args.accounts)}{util.Typgpy.ENDC}")
 
     if not args.yes and input("Fund accounts?\n[Y]/n > ") != 'Y':
         exit()
@@ -234,7 +306,7 @@ if __name__ == "__main__":
             threads.clear()
             for _ in range(os.cpu_count()):
                 i = next(shard_iter)
-                threads.append(pool.apply_async(fund, (i,)))
+                threads.append(pool.apply_async(fund, (i, csv_data)))
             for t in threads:
                 t.get()
     except StopIteration:
@@ -244,18 +316,26 @@ if __name__ == "__main__":
 
     print(f"{util.Typgpy.HEADER}Finished sending transactions!{util.Typgpy.ENDC}")
     if args.check:
+        if csv_data:
+            check_data = random.sample(csv_data, max(len(csv_data) // 10, 1))
+        else:
+            check_data = []
+            for addr in random.sample(args.accounts, max(len(args.accounts) // 10, 1)):
+                check_data.append({
+                    'amount': args.amount,
+                    'address': addr
+                })
         print(f"{util.Typgpy.HEADER}Sleeping 60 seconds before checking balances{util.Typgpy.ENDC}")
         time.sleep(60)
-        addrs_to_check = random.sample(args.accounts, max(len(args.accounts) // 10, 1))
-        print(f"{util.Typgpy.HEADER}Spot checking {len(addrs_to_check)} balances....{util.Typgpy.ENDC}")
+        print(f"{util.Typgpy.HEADER}Spot checking {len(check_data)} balances....{util.Typgpy.ENDC}")
         failed = False
-        for addr in addrs_to_check:
-            for bal in get_balance_from_node_ip(addr, endpoints):
-                if int(bal["shard"]) in args.shards and float(bal["amount"]) < float(args.amount):
-                    print(f"{util.Typgpy.FAIL}{addr} did not get funded!{util.Typgpy.ENDC}")
+        for dat in check_data:
+            for bal in get_balance_from_node_ip(dat['address'], endpoints):
+                if int(bal["shard"]) in args.shards and float(bal["amount"]) < float(dat["amount"]):
+                    print(f"{util.Typgpy.FAIL}{dat['address']} did not get funded!{util.Typgpy.ENDC}")
                     failed = True
                     break
         if not failed:
-            print(f"{util.Typgpy.HEADER}Successfully checked {len(addrs_to_check)} balances....{util.Typgpy.ENDC}")
+            print(f"{util.Typgpy.HEADER}Successfully checked {len(check_data)} balances....{util.Typgpy.ENDC}")
         else:
             exit(-1)
