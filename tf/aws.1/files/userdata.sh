e868e185acb7d5a69f8342757b3f920224e170ae
Leo Chen <leo@harmony.one>
2020-04-21 10:04:48 +0000

    [tf] fix userdata.sh for aws
    Signed-off-by: Leo Chen <leo@harmony.one>

--- tf/aws.1/files/userdata.sh
+++ tf/aws.1/files/userdata.sh
@@ -9,5 +9,5 @@ curl https://rclone.org/install.sh | sudo bash
 mkdir -p /home/ec2-user/.hmy
 mkdir -p /home/ec2-user/.config/rclone
 
-chown -R ec2-user .hmy
-chown -R ec2-user .config
+chown -R ec2-user /home/ec2-user/.hmy
+chown -R ec2-user /home/ec2-user/.config
