47e0a99eab74f0e890d9c83205f67328f31e8b8c
Leo Chen <leo@harmony.one>
2020-04-22 19:11:55 +0000

    [tf/aws] move node_exporter installation to userdata
    Signed-off-by: Leo Chen <leo@harmony.one>

--- tf/aws.1/files/userdata.sh
+++ tf/aws.1/files/userdata.sh
@@ -11,3 +11,9 @@ mkdir -p /home/ec2-user/.config/rclone
 
 chown -R ec2-user /home/ec2-user/.hmy
 chown -R ec2-user /home/ec2-user/.config
+
+curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+tar xfz node_exporter-0.18.1.linux-amd64.tar.gz
+mv -f node_exporter-0.18.1.linux-amd64/node_exporter /usr/local/bin
+rm -rf node_exporter-0.18.1.linux-amd64.tar.gz node_exporter-0.18.1.linux-amd64
+useradd -rs /bin/false node_exporter
