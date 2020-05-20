6528e88c0d8ca909e78614adfc8495a76d2fb354
Leo Chen <leo@harmony.one>
2020-05-20 01:06:04 -0700

    [ansible] add jacky in user playbook
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ ansible/scripts/users.sh
@@ -0,0 +1,4 @@
+ansible-playbook playbooks/users.yml -e 'inventory=tdevop' -i inventory/hmy.hosts
+ansible-playbook playbooks/users.yml -e 'inventory=watchdog' -i inventory/hmy.hosts
+ansible-playbook playbooks/users.yml -e 'inventory=jenkins' -i inventory/hmy.hosts
+# ansible-playbook playbooks/users.yml -e 'inventory=devop' -i inventory/hmy.hosts
