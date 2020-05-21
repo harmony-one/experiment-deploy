b40059690244217d96b65ec6cce0fb3bbfaba376
Leo Chen <leo@harmony.one>
2020-05-21 01:10:13 -0700

    [ansible] playbook to install harmony repo
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ ansible/scripts/devhost.sh
@@ -0,0 +1,2 @@
+ansible-playbook playbooks/devhost.yml -e 'inventory=arm user=ec2-user' -i inventory/hmy.hosts
+# ansible-playbook playbooks/install-golang.yml -e 'inventory=arm user=ec2-user' -i inventory/hmy.hosts
