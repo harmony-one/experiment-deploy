644baed59b023af0713f565b0d0f27c785537f3b
Leo Chen <leo@harmony.one>
2020-05-21 20:00:34 +0000

    [r53] set TTL to 300 for DNS records
    Signed-off-by: Leo Chen <leo@harmony.one>

--- pipeline/r53update.py
+++ pipeline/r53update.py
@@ -226,7 +226,7 @@ def main():
             'ResourceRecordSet': {
                 'Name': f'{shard_label}.{zone_name}',
                 'Type': rrtype_by_version[version],
-                'TTL': 3600,
+                'TTL': 300,
                 'ResourceRecords': [{'Value': str(addr)} for addr in addrs],
             },
         })
@@ -238,7 +238,7 @@ def main():
             'ResourceRecordSet': {
                 'Name': f'{leader_label}.{zone_name}',
                 'Type': rrtype_by_version[leader.version],
-                'TTL': 3600,
+                'TTL': 300,
                 'ResourceRecords': [{'Value': str(leader)}],
             },
         })
