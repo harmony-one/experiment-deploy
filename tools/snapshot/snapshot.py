d06eebb4cdcf4415698e4d81c53f472181f5401c
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-05-24 06:33:21 -0700

    [snapshot] Add snapshotting main logic
    * Refactor `ips` to `machines`
    * Make sanity checks faster by parallelizing as much as possible
    * Add todos for programmatic verification of rclone setup & cleanup
    * Add max_wait condition to config

--- tools/snapshot/snapshot.py
+++ tools/snapshot/snapshot.py
@@ -35,7 +35,8 @@ from pyhmy import (
 pyhmy_version = '20.5.5'
 script_directory = os.path.dirname(os.path.realpath(__file__))
 log = logging.getLogger("snapshot")
-ips, rsync, ssh_key, condition = [], {}, {}, {}  # Will be populated from config.
+beacon_chain_shard = 0
+machines, rsync, ssh_key, condition = [], {}, {}, {}  # Will be populated from config.
 
 
 def _init_ssh_agent():
@@ -69,7 +70,7 @@ def _ssh_cmd(user, ip, command):
     return subprocess.check_output(cmd, env=os.environ).decode()
 
 
-def _ssh_script(user, ip, script_path):
+def _ssh_script(user, ip, bash_script_path):
     """
     Internal SSH command. Assumes ssh agent has been initialized.
 
@@ -78,7 +79,7 @@ def _ssh_script(user, ip, script_path):
     """
     cmd = ["ssh", f"{user}@{ip}"] if ssh_key['use_existing_agent'] else ["ssh", "-i", ssh_key["path"], f"{user}@{ip}"]
     cmd.append('bash -s')
-    with open(script_path, 'rb') as f:
+    with open(bash_script_path, 'rb') as f:
         return subprocess.check_output(cmd, env=os.environ, stdin=f).decode()
 
 
@@ -101,14 +102,14 @@ def load_config(config_path):
     """
     with open(config_path, 'r', encoding="utf-8") as f:
         config = json.load(f)
-    if {'ips', 'ssh_key', 'rsync', 'condition'} != set(config.keys()):
-        raise KeyError(f"config keys: {config.keys()} do not contain 'ips', 'ssh_key', 'condition' or 'rsync'.")
+    if {'machines', 'ssh_key', 'rsync', 'condition'} != set(config.keys()):
+        raise KeyError(f"config keys: {config.keys()} do not contain 'machines', 'ssh_key', 'condition' or 'rsync'.")
     log.debug(json.dumps(config))
-    ips.clear()
+    machines.clear()
     rsync.clear()
     ssh_key.clear()
     condition.clear()
-    ips.extend(config['ips'])
+    machines.extend(config['machines'])
     rsync.update(config['rsync'])
     ssh_key.update(config['ssh_key'])
     condition.update(config['condition'])
@@ -126,29 +127,31 @@ def sanity_check():
         log.warning('force snapshot, bypassing sanity check...')
         return
 
-    threads, pool = [], ThreadPool()
-    for ip in map(lambda e: e['ip'], ips):
-        threads.append(pool.apply_async(is_active_shard, (f"http://{ip}:9500/", condition['last_block_tolerance'])))
-    if not all(t.get() for t in threads):
-        raise RuntimeError(f"One or more of the configured IPs are either offline "
-                           f"or latest block is older than {condition['last_block_tolerance']} seconds")
-    log.debug(f"all configured ips are active & synced within tolerance of {condition['last_block_tolerance']} sec")
+    config_shard_set = set(map(lambda e: e['shard'], machines))
+    if len(config_shard_set) != len(machines):
+        raise RuntimeError(f"Number of unique shards does not equal number of configured machines. "
+                           f"Snapshot only supports 1 machines per shard.")
+    if beacon_chain_shard not in config_shard_set:
+        raise RuntimeError(f"config does not specify beacon chain ({beacon_chain_shard})")
 
-    threads = []
-    for ip_config in ips:
+    threads, pool = [], ThreadPool()
+    for machine in machines:
         def fn():  # returning None marks success for this function
             try:
-                node_metadata = blockchain.get_node_metadata(f"http://{ip_config['ip']}:9500/", timeout=15)
+                node_metadata = blockchain.get_node_metadata(f"http://{machine['ip']}:9500/", timeout=15)
             except (rpc_exceptions.RPCError, rpc_exceptions.RequestsTimeoutError, rpc_exceptions.RequestsError) as e:
                 log.error(traceback.format_exc())
-                return f"errored when fetching metadata for {ip_config['ip']}. Error {e}"
+                return f"errored when fetching metadata for {machine['ip']}. Error {e}"
             shard, role, network = node_metadata['shard-id'], node_metadata['role'], node_metadata['network']
-            if int(shard) != int(ip_config['shard']):
-                return f"configured shard {ip_config['shard']} != actual node shard of {shard}. (ip: {ip_config['ip']})"
+            if int(shard) != int(machine['shard']):
+                return f"configured shard {machine['shard']} != actual node shard of {shard}. (ip: {machine['ip']})"
             if condition['role'] != role:
-                return f"configured node role {condition['role']} != actual node role of {role}. (ip: {ip_config['ip']})"
+                return f"configured node role {condition['role']} != actual node role of {role}. (ip: {machine['ip']})"
             if condition['network'] != network:
-                return f"configured node network {condition['network']} != actual node network of {network}. (ip: {ip_config['ip']})"
+                return f"configured node network {condition['network']} != actual node network of {network}. (ip: {machine['ip']})"
+            if not is_active_shard(f"http://{machine['ip']}:9500/", condition['max_seconds_since_last_block']):
+                return f"one or more of the configured IPs are either offline " \
+                       f"or latest block is older than {condition['max_seconds_since_last_block']} seconds"
             return None  # indicate success
         threads.append(pool.apply_async(fn))
     for t in threads:
@@ -158,40 +161,16 @@ def sanity_check():
     log.debug('passed sanity check')
 
 
-def _setup_rclone(ip_config, script_path):
+def _setup_rclone(machine, bash_script_path):
     """
     Worker to setup rclone on machine.
     """
-    log.debug(f"setting up rclone snapshot credentials for {ip_config['ip']}")
-    setup_response = _ssh_script(ip_config['user'], ip_config['ip'], script_path)
+    log.debug(f"setting up rclone snapshot credentials for {machine['ip']}")
+    setup_response = _ssh_script(machine['user'], machine['ip'], bash_script_path)
     log.debug(f"rsync snapshot credentials setup response: {setup_response}")
-    verification_cat = _ssh_cmd(ip_config['user'], ip_config['ip'], f"cat {rsync['config_path_on_client']}")
-    log.debug(f"rsync snapshot credentials on machine: {ip_config['ip']}: {verification_cat}")
-
-
-def _cleanup_rclone(ip_config):
-    """
-    Worker to cleanup rclone on machine.
-    """
-    log.debug(f"cleaning up rclone snapshot credentials for {ip_config['ip']}")
-    cleanup_response = _ssh_cmd(ip_config['user'], ip_config['ip'], f"rm {rsync['config_path_on_client']}")
-    log.debug(f"rsync snapshot credentials cleanup response: {cleanup_response}")
-    verification_cat = _ssh_cmd(ip_config['user'], ip_config['ip'], f"[ -f {rsync['config_path_on_client']} ] || echo removed file!")
-    log.debug(f"rsync snapshot credentials cleanup check: {ip_config['ip']}: {verification_cat}")
-
-
-def cleanup_rclone():
-    """
-    Clean/remove rclone credentials that were added (as dictated by the config).
-
-    Note that this will remove the file at whatever the `config_path_on_client`
-    specifies in the config.
-    """
-    threads, pool = [], ThreadPool()
-    for ip_config in ips:
-        threads.append(pool.apply_async(_cleanup_rclone, (ip_config,)))
-    for t in threads:
-        t.get()
+    # TODO: programmatic verification
+    verification_cat = _ssh_cmd(machine['user'], machine['ip'], f"cat {rsync['config_path_on_client']}")
+    log.debug(f"rsync snapshot credentials on machine: {machine['ip']}: {verification_cat}")
 
 
 def setup_rclone():
@@ -203,17 +182,160 @@ def setup_rclone():
     threads, pool = [], ThreadPool()
     with open(rsync['config_path_on_host'], 'r') as f:
         rclone_config_raw_string = f.read()
-    script_content = f"""#!/bin/bash
+    bash_script_content = f"""#!/bin/bash
 echo "{rclone_config_raw_string}" > {rsync['config_path_on_client']}
 """
-    script_path = f"/tmp/snapshot_script{hash(time.time())}.sh"
-    with open(script_path, 'w') as f:
-        f.write(script_content)
-    for ip_config in ips:
-        threads.append(pool.apply_async(_setup_rclone, (ip_config, script_path)))
+    bash_script_path = f"/tmp/snapshot_script{hash(time.time())}.sh"
+    with open(bash_script_path, 'w') as f:
+        f.write(bash_script_content)
+    try:
+        for machine in machines:
+            threads.append(pool.apply_async(_setup_rclone, (machine, bash_script_path)))
+        for t in threads:
+            t.get()
+    finally:
+        os.remove(bash_script_path)
+
+
+def _cleanup_rclone(machine):
+    """
+    Worker to cleanup rclone on machine.
+    """
+    log.debug(f"cleaning up rclone snapshot credentials for {machine['ip']}")
+    cleanup_response = _ssh_cmd(machine['user'], machine['ip'], f"rm {rsync['config_path_on_client']}")
+    log.debug(f"rsync snapshot credentials cleanup response: {cleanup_response}")
+    # TODO: programmatic verification
+    cmd = f"if [ -f {rsync['config_path_on_client']} ]; then echo failed cleanup; else echo successful cleanup; fi"
+    verification_cat = _ssh_cmd(machine['user'], machine['ip'], cmd)
+    log.debug(f"rsync snapshot credentials cleanup check: {machine['ip']}: {verification_cat}")
+
+
+def cleanup_rclone():
+    """
+    Clean/remove rclone credentials that were added (as dictated by the config).
+
+    Note that this will remove the file at whatever the `config_path_on_client`
+    specifies in the config.
+    """
+    threads, pool = [], ThreadPool()
+    for machine in machines:
+        threads.append(pool.apply_async(_cleanup_rclone, (machine,)))
     for t in threads:
         t.get()
-    os.remove(script_path)
+
+
+def _bucket_sync(machine):
+    """
+    Internal function to start bucket sync.
+    Function call will block until bucket sync is done on machine.
+    """
+    log.debug(f'starting bucket sync on {machine["ip"]}')
+
+
+def _local_sync(machine):
+    """
+    Internal function to trigger a local sink.
+    Function call will block until local sync is done on machine.
+    """
+    log.debug(f'starting local sync on {machine["ip"]}')
+
+
+def _stop_harmony(machine):
+    """
+    Internal function to stop and verify harmony service.
+    """
+    log.debug(f'stopping harmony service on {machine["ip"]}')
+    machine_stop_response = _ssh_cmd(machine['user'], machine['ip'], "sudo systemctl stop harmony")
+    log.debug(f'stop cmd response: {machine_stop_response}')
+    off_msg = "SERVICE_STOPPED"
+    cmd = f"if systemctl is-active --quiet harmony; then echo SERVICE_STARTED; else echo {off_msg}; fi"
+    off_msg_response = _ssh_cmd(machine['user'], machine['ip'], cmd).strip()
+    if off_msg != off_msg_response:
+        log.error("harmony service failed to stop")
+        log.error(f"expected msg response: {off_msg}, got: {off_msg_response}")
+        raise RuntimeError("harmony service failed to stop")
+    log.debug(f'successfully stopped harmony service on {machine["ip"]}')
+
+
+def _start_harmony(machine):
+    """
+    Internal function to start and verify harmony service.
+    """
+    log.debug(f'starting harmony service on {machine["ip"]}')
+    machine_start_resposne = _ssh_cmd(machine['user'], machine['ip'], "sudo systemctl start harmony")
+    log.debug(f'start cmd response: {machine_start_resposne}')
+    on_msg = "SERVICE_STARTED"
+    cmd = f"if systemctl is-active --quiet harmony; then echo {on_msg}; else echo SERVICE_STOPPED; fi"
+    on_msg_response = _ssh_cmd(machine['user'], machine['ip'], cmd).strip()
+    if on_msg != on_msg_response:
+        log.error("harmony service failed to start")
+        log.error(f"expected msg response: {on_msg}, got: {on_msg_response}")
+        raise RuntimeError("harmony service failed to start")
+    log.debug(f'successfully started harmony service on {machine["ip"]}')
+
+
+def _snapshot_beacon(machine):
+    """
+    Internal worker to snapshot the beacon shard.
+    """
+    assert machine['shard'] == beacon_chain_shard, f'wrong machine for beacon snapshot: {machine}'
+    log.debug(f'started beacon snapshot ({machine["ip"]})')
+    _stop_harmony(machine)
+    _local_sync(machine)
+    bucket_sync_thread = ThreadPool().apply_async(_bucket_sync, (machine, ))
+    post_delay = 10  # Sleep after bucket sync so aux shard nodes can detect that beacon node is down
+    log.debug(f'sleeping {post_delay} seconds before restarting node')
+    time.sleep(post_delay)
+    _start_harmony(machine)
+    bucket_sync_thread.get()
+    log.debug(f'finished beacon snapshot ({machine["ip"]})')
+
+
+def _snapshot_aux(machine, beacon_endpoint):
+    """
+    Internal worker to snapshot aux shard
+    Assume that `beacon_endpoint` is correct (no validation can be done at this stage)
+    """
+    assert machine['shard'] != beacon_chain_shard, f'wrong machine for aux snapshot: {machine}'
+    log.debug(f'started aux snapshot ({machine["ip"]})')
+    init_delay = 3  # Sleep so beacon snapshots first
+    log.debug(f'sleeping {init_delay} seconds before snapshotting')
+    time.sleep(init_delay)
+    try:
+        count, start_time = 0, time.time()
+        while time.time() - start_time <= condition['max_seconds_wait_for_beacon']:
+            blockchain.get_node_metadata(beacon_endpoint, timeout=10)
+            count += 1
+            if count % 25 == 0:
+                log.warning(f'waiting for beacon to terminate before starting snapshot, tried {count} times')
+    except (rpc_exceptions.RequestsError, rpc_exceptions.RequestsTimeoutError):
+        log.debug(f'beacon {beacon_endpoint} offline, snapshotting now...')
+    _stop_harmony(machine)
+    _local_sync(machine)
+    _start_harmony(machine)
+    _bucket_sync(machine)
+    log.debug(f'finished aux snapshot ({machine["ip"]})')
+
+
+def snapshot():
+    """
+    Execute the snapshot of the network using the given config.
+    Assumes that rclone for configured `snapshot_bin` is setup on configured `machines`.
+    Assumes that `sanity_check` was ran before this is called.
+
+    Note that beacon chain will shutdown & snapshot FIRST in-order to guarantee
+    that crosslinks are clean. Moreover, beacon chain is NECESSARY to generate a
+    snapshot a network.
+    """
+    log.debug('started snapshot')
+    beacon_machine = list(filter(lambda e: e['shard'] == beacon_chain_shard, machines))[0]
+    aux_machines = filter(lambda e: e['shard'] != beacon_chain_shard, machines)
+    threads, pool = [], ThreadPool()
+    threads.append(pool.apply_async(_snapshot_beacon, (beacon_machine,)))
+    for machine in aux_machines:
+        threads.append(pool.apply_async(_snapshot_aux, (machine, f"http://{beacon_machine['ip']}:9500/")))
+    for t in threads:
+        t.get()
 
 
 def _parse_args():
@@ -235,7 +357,8 @@ if __name__ == "__main__":
         log.debug("initialized snapshot script")
         sanity_check()
         setup_rclone()
-        # TODO: Copy over logic from old script & test snapshot
+
+
         cleanup_rclone()
     except Exception as e:
         log.fatal(traceback.format_exc())
