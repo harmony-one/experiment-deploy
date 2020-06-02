43e573aa22322dd1bc9a9bbbaffe5b262f8eeeb9
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-06-02 01:23:15 -0700

    [snapshot] Implement interactive input & arg parse
    * Add sanity checks on arg parse

--- pipeline/snapshot_recover.py
+++ pipeline/snapshot_recover.py
@@ -25,6 +25,7 @@ import sys
 import json
 import logging
 import traceback
+import re
 from multiprocessing.pool import ThreadPool
 
 import pexpect
@@ -43,35 +44,77 @@ from pyhmy import (
     blockchain,
 )
 
-
 script_directory = os.path.dirname(os.path.realpath(__file__))
-log = logging.getLogger("snapshot")
+log = logging.getLogger("snapshot_recovery")
+supported_networks = {"mainnet", "testnet", "staking", "partner", "stress"}
 beacon_chain_shard = 0
 
-
-def parse_args():
-    parser = argparse.ArgumentParser(description='Recover snapshot script to be ran from devops machine')
-    parser.add_argument("log_dir", type=str, help="Path to the log directory that contains the networks's shard?.txt files.")
-    parser.add_argument("--network", type=str, default="mainnet", help="Network type (default: mainnet)")
-    parser.add_argument("--shard", type=str, default="0,1,2,3",
-                        help="String in CSV format of shards to recover, default is '0,1,2,3'")
-    return parser.parse_args()
+_ip_regex = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
 
 
 def setup_logger(do_print=True):
     """
     Setup the logger for the snapshot package and returns the logger.
     """
-    logger = logging.getLogger("snapshot")
-    file_handler = logging.FileHandler(f"{script_directory}/snapshot.log")
+    log_file = f"{script_directory}/logs/{os.environ['HMY_PROFILE']}/snapshot_recovery.log"
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+    logger = logging.getLogger("snapshot_recovery")
+    file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(
         logging.Formatter(f"(%(threadName)s)[%(asctime)s] %(message)s"))
     logger.addHandler(file_handler)
     if do_print:
         logger.addHandler(logging.StreamHandler(sys.stdout))
     logger.setLevel(logging.DEBUG)
-    logger.debug("===== NEW SNAPSHOT =====")
+    logger.debug("===== NEW RECOVERY =====")
+    print(f"Logs saved to: {log_file}")
     return logger
+
+
+def interact(prompt, selection_list):
+    """
+    The single source of interaction with the console.
+    All interaction must confine to this function's requirement.
+
+    Prompt the user with `prompt` and an enumerated selection from a sorted `selection_list`.
+    Take in an integer, n, such that 0 <= n < len(`selection_list`).
+    If a `log` is provided, log the interaction and all errors at the info and error level respectively.
+
+    Keeps prompting user for input if input is invalid.
+    Prints user interaction before returning.
+
+    Note that all new lines from `prompt` and `selection_list` will be removed.
+
+    Returns n and corresponding selection string from `selection_list`.
+    """
+    input_prompt = f"{Typgpy.BOLD}Select option (number):{Typgpy.ENDC}\n> "
+    prompt, selection_list = prompt.replace("\n", ""), sorted(map(lambda e: e.replace("\n", ""), selection_list))
+    prompt_new_line_count = sum(1 for el in selection_list if el) + 3  # 1 for given prompt, 2 for input prompt
+    if prompt:
+        prompt_new_line_count += 1
+    printed_new_line_count = 0
+    print()
+
+    while True:
+        if prompt:
+            print(prompt)
+        for i, selection in enumerate(selection_list):
+            print(f"{Typgpy.BOLD}[{i}]{Typgpy.ENDC}\t{selection}")
+        user_input = input(input_prompt)
+        printed_new_line_count += prompt_new_line_count
+        try:
+            n = int(user_input)
+            if n >= len(selection_list):
+                continue
+            selection_report = f"{prompt} {Typgpy.BOLD}[{n}]{Typgpy.ENDC} {selection_list[n]}".strip()
+            for i in range(printed_new_line_count):
+                sys.stdout.write("\033[K")
+                if i + 1 < printed_new_line_count:
+                    sys.stdout.write("\033[F")
+            print(selection_report)
+            return selection_list[n]
+        except ValueError:
+            pass
 
 
 def select_snapshot():
@@ -129,8 +172,9 @@ def restart_and_check(ips_per_shard):
     """
     Main restart and verification function after DBs have been restored.
     """
-    if input(f"Restart shards {sorted(ips_per_shard.keys())}? [Y/n]\n> ").lower() not in {'yes', 'y'}:
+    if interact(f"Restart shards: {sorted(ips_per_shard.keys())}?", ["yes", "no"]) == "no":
         return
+
     threads = []
     post_check_pool = ThreadPool()
     for shard in ips_per_shard.keys():
@@ -168,31 +212,119 @@ def restart_and_check(ips_per_shard):
 
 def _get_ips_per_shard(args):
     """
-    Internal function to get the IPs per shard given a parsed args.
+    Internal function to get the IPs per shard given a parsed args, only used for main execution.
     """
-    return {}
+    log.debug("Loading IPs from given directory...")
+
+    ips_per_shard = {}
+    for file in os.listdir(args.logs_dir):
+        if re.match(r"shard[0-9]+.txt", file):
+            shard = int(file.replace("shard", "").replace(".txt", ""))
+            if shard in ips_per_shard.keys():
+                raise RuntimeError(f"Multiple IP files for shard {shard}")
+            with open(f"{args.logs_dir}/{file}", 'r', encoding='utf-8') as f:
+                ips = [line.strip() for line in f.readlines() if re.search(_ip_regex, line)]
+            if not ips:
+                raise RuntimeError(f"no VALID IP was loaded from file: '{args.logs_dir}/{file}'")
+            log.debug(f"Candidate IPs for shard {shard}: {ips}")
+
+            print(f"\nShard {Typgpy.HEADER}{shard}{Typgpy.ENDC} ips ({len(ips)}):")
+            for i, ip in enumerate(ips):
+                print(f"{i + 1}.\t{Typgpy.OKGREEN}{ip}{Typgpy.ENDC}")
+            choices = [
+                "Add all IPs",
+                "Select IPs to add (interactively)",
+                "Ignore"
+            ]
+            response = interact("", choices)
+
+            if response == choices[-1]:
+                log.debug(f"ignoring IPs from shard {shard}")
+                continue
+            if response == choices[0]:
+                log.debug(f"shard {shard} IPs: {ips}")
+                ips_per_shard[shard] = ips
+                continue
+            if response == choices[1]:
+                selected_ips = []
+                for ip in ips:
+                    prompt = f"Add {Typgpy.OKGREEN}{ip}{Typgpy.ENDC} for shard {Typgpy.HEADER}{shard}{Typgpy.ENDC}?"
+                    if interact(prompt, ["yes", "no"]) == "yes":
+                        selected_ips.append(ip)
+                if not selected_ips:
+                    msg = f"selected 0 IPs for shard {shard}, ignoring shard"
+                    log.debug(msg)
+                    continue
+                print(f"\nShard {Typgpy.HEADER}{shard}{Typgpy.ENDC} "
+                      f"{Typgpy.UNDERLINE}selected{Typgpy.ENDC} ips ({len(selected_ips)})")
+                for i, ip in enumerate(selected_ips):
+                    print(f"{i + 1}.\t{Typgpy.OKGREEN}{ip}{Typgpy.ENDC}")
+                if interact(f"Add ips for shard {Typgpy.HEADER}{shard}{Typgpy.ENDC}?", ["yes", "no"]) == "yes":
+                    log.debug(f"shard {shard} IPs: {selected_ips}")
+                    ips_per_shard[shard] = selected_ips
+                else:
+                    log.debug(f"ignoring IPs from shard {shard}")
+                continue
+
+    for shard in sorted(ips_per_shard.keys()):
+        print(f"Shard {Typgpy.HEADER}{shard}{Typgpy.ENDC} {Typgpy.UNDERLINE}loaded{Typgpy.ENDC} IPs:")
+        print('-' * 16)
+        for ip in sorted(ips_per_shard[shard]):
+            print(ip)
+        print()
+
+    final_report = "Added "
+    for k, v in ips_per_shard.items():
+        final_report += f"{len(v)} ips for shard {k}; "
+    log.debug(final_report)
+    return ips_per_shard
+
+
+def _assumption_check(args):
+    """
+    Internal function that checks the assumptions of the script, only used for main execution.
+    """
+    assert args.network in supported_networks, f"given network must be one of {supported_networks}"
+    assert os.path.isfile(f"{script_directory}/node_ssh.sh"), "script not in pipeline directory"
+    assert os.path.isdir(args.logs_dir), "given logs directory is not a directory"
+    if not any(f for f in os.listdir(args.logs_dir) if re.match(r"shard[0-9]+.txt", f)):
+        raise AssertionError(f"expected given logs directory, {args.logs_dir} to contain a shard?.txt file")
+    assert os.path.isfile(args.rclone_config_path), "given rclone config file path is not a file"
+    assert re.match(r".*:.*", args.snapshot_bin), "given snapshot bin does not follow format: <rclone-config>:<bin>"
 
 
 def _parse_args():
     """
-    Argument parser that is only used for main execution of this script.
+    Argument parser that is only used for main execution.
     """
-    parser = argparse.ArgumentParser(description='Snapshot script to be ran from command machine')
-    default_config_path = f"{script_directory}/config.json"
-    parser.add_argument("--config", type=str, default=default_config_path,
-                        help=f"path to snapshot config (default {default_config_path})")
-    parser.add_argument("--bucket-sync", action='store_true',
-                        help="Enable syncing to external bucket (where bucket is defined in the config)")
-    args = parser.parse_args()
-    return _get_ips_per_shard(args), args
+    if 'HMY_PROFILE' not in os.environ:
+        raise SystemExit("HMY_PROFILE not set, exiting...")
+    parser = argparse.ArgumentParser(description='Snapshot recovery script')
+    parser.add_argument("network", type=str, help=f"{supported_networks}")
+    default_log_dir = f"{script_directory}/logs/{os.environ['HMY_PROFILE']}"
+    parser.add_argument("--logs-dir", type=str,
+                        help="the logs directory containing the shard?.txt files needed to ssh into the machines, "
+                             f"default is '{default_log_dir}'")
+    default_rclone_config_path = f"{script_directory}/../tools/snapshot/rclone.conf"
+    parser.add_argument("--rclone-config-path", type=str, default=default_rclone_config_path,
+                        help="path to rclone config to download the snapshot db, "
+                             f"default is '{default_rclone_config_path}'")
+    parser.add_argument("--snapshot-bin", type=str, default=f"snapshot:harmony-snapshot",
+                        help="the rclone config name (based on `--rclone-config`) and bin to download the snapshot db, "
+                             "default is 'snapshot:harmony-snapshot'")
+    parser.add_argument("--verbose", action="store_true")
+    return parser.parse_args()
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     assert pyhmy.__version__.major == 20
     assert pyhmy.__version__.minor >= 5
     assert pyhmy.__version__.micro >= 5
-    ips_per_shard, args = _parse_args()
-    setup_logger()
+    args = _parse_args()
+    _assumption_check(args)
+    if args.verbose:
+        setup_logger(do_print=True)
+    ips_per_shard = _get_ips_per_shard(args)
     reset_dbs_interactively(ips_per_shard)
     restart_and_check(ips_per_shard)
     log.debug("finished recovery")
