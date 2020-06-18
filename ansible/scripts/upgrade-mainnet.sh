5340cbfdc1a08f8f9bb2143e361a5df665a921d1
Leo Chen <leo@harmony.one>
2020-06-18 08:20:56 +0000

    a helper script to do rolling upgrade of mainnet
    Signed-off-by: Leo Chen <leo@harmony.one>

--- ansible/scripts/upgrade-mainnet.sh
+++ ansible/scripts/upgrade-mainnet.sh
@@ -5,16 +5,16 @@ PS3="Select the operation: "
 select opt in rolling_upgrade force_upgrade quit; do
    case $opt in
       rolling_upgrade)
-         read -p "The group (p2ps{0..1}/p2p): " shard
+         read -p "The group (s{0..3}/s{0..3}_canary/canary): " shard
          read -p "The release bucket: " release
          ansible-playbook playbooks/upgrade-node.yml -e "inventory=${shard} stride=1 upgrade=${release}"
          echo "NOTED: the leader won't be upgraded. Please upgrade leader with force_update=true"
          ;;
       force_upgrade)
-         read -p "The group (p2ps{0..1}/p2p): " shard
+         read -p "The group (s{0..3}/s{0..3}_canary/canary): " shard
          read -p "The release bucket: " release
          # force upgrade and no consensus check
-         ANSIBLE_STRATEGY=free ansible-playbook playbooks/upgrade-node.yml -f 20 -e "inventory=${shard} stride=20 upgrade=${release} force_update=true skip_consensus_check=true"
+         ANSIBLE_STRATEGY=free ansible-playbook playbooks/upgrade-node.yml -f 10 -e "inventory=${shard} stride=10 upgrade=${release} force_update=true skip_consensus_check=true"
          ;;
       quit)
          break
