8a59f0c336500ca0ffdabd3535e33f21caee9c49
Leo Chen <leo@harmony.one>
2020-06-25 08:33:13 +0000

    [ansible] improve restart-node.yml
    Signed-off-by: Leo Chen <leo@harmony.one>

--- ansible/scripts/upgrade-mainnet.sh
+++ ansible/scripts/upgrade-mainnet.sh
@@ -12,7 +12,9 @@ select opt in rolling_upgrade restart force_upgrade quit; do
          ;;
       restart)
          read -p "The node group or node IP: " shard
-         ANSIBLE_STRATEGY=free ansible-playbook playbooks/restart-node.yml -f 50 -e "inventory=${shard} stride=50"
+         read -p "Number of nodes in one batch (1-50): " batch
+         read -p "Skip checking of consensus (true/false): " skip
+         ANSIBLE_STRATEGY=free ansible-playbook playbooks/restart-node.yml -f $batch -e "inventory=${shard} stride=${batch} skip_consensus_check=${skip}"
          ;;
       force_upgrade)
          read -p "The group (s{0..3}/s{0..3}_canary/canary): " shard
